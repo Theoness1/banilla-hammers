35aa20aa0037590580bfc00f9dfca3f9f1332406
Theoness1 <sasha.bolotskih2002@gmail.com>
2023-09-14 23:51:49 +0700

    added ItemGroup

--- src/main/java/ru/theone_ss/banilla_hammers/BanillaHammers.java
+++ src/main/java/ru/theone_ss/banilla_hammers/BanillaHammers.java
@@ -1,7 +1,12 @@
 package ru.theone_ss.banilla_hammers;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import ru.theone_ss.banilla_hammers.compat.WinterlyIntegration;
 import ru.theone_ss.banilla_hammers.registry.BanillaHammersEffects;
@@ -10,10 +15,12 @@ import ru.theone_ss.banilla_hammers.registry.BanillaHammersItems;
 import ru.theone_ss.banilla_hammers.registry.BanillaHammersParticles;
 
 public class BanillaHammers implements ModInitializer {
+	public static ItemGroup BANILLA_HAMMERS;
 	public static final String MOD_ID = "banilla_hammers";
 
 	@Override
 	public void onInitialize() {
+		BANILLA_HAMMERS = createItemGroup();
 		BanillaHammersItems.init();
 		BanillaHammersEntities.init();
 		BanillaHammersEffects.init();
@@ -21,6 +28,14 @@ public class BanillaHammers implements ModInitializer {
 		if(FabricLoader.getInstance().isModLoaded("winterly")) {
 			WinterlyIntegration.init();
 		}
+	}
+
+	private static ItemGroup createItemGroup() {
+		var group = FabricItemGroup.builder().displayName(Text.translatable("Banilla Hammers"))
+				.icon(() -> BanillaHammersItems.DIAMOND_HAMMER.asItem().getDefaultStack())
+				.entries((displayContext, entries) -> BanillaHammersItems.ITEMS.forEach((id, item) -> entries.add(item.getDefaultStack()))).build();
+		Registry.register(Registries.ITEM_GROUP, id("items"), group);
+		return group;
 	}
 
 	public static Identifier id(String path) {
